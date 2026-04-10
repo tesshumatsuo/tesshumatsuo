1e2a4fac80a6a6234458468280e9d3de6b1ebca0
tesshumatsuo <tesshu@tesshumatsuo.com>
2026-04-10 15:17:36 +0900

    fix: change Spotify player height to 200px for proper compact display

--- src/app/(main)/[locale]/blog/[slug]/page.tsx
+++ src/app/(main)/[locale]/blog/[slug]/page.tsx
@@ -251,7 +251,7 @@ const ptComponents: any = {
             style={{ borderRadius: '12px' }} 
             src={`https://open.spotify.com/embed/${embedId}?utm_source=generator`} 
             width="100%" 
-            height="152" 
+            height="200" 
             lang="en"
             frameBorder="0" 
             allowFullScreen={false} 
@@ -366,7 +366,7 @@ const ptComponents: any = {
                 style={{ borderRadius: '12px' }}
                 src={`https://open.spotify.com/embed/${embedId}?utm_source=generator`}
                 width="100%"
-                height="152"
+                height="200"
                 frameBorder="0"
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 loading="lazy"
@@ -434,7 +434,7 @@ const ptComponents: any = {
                 style={{ borderRadius: '12px' }}
                 src={`https://open.spotify.com/embed/${embedId}?utm_source=generator`}
                 width="100%"
-                height="152"
+                height="200"
                 frameBorder="0"
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 loading="lazy"
